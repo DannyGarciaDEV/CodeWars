a5f68151fb18dd36c692995b308ad9347302bfa5
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-03-24 11:53:34 -0400

    new codewars

--- kyu 8/main.js
+++ kyu 8/main.js
@@ -65,6 +65,15 @@ function multiply(a, b){
     
   }
 
+
+// monday 20 march 2023 
+//Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+//[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+function grow(x){
+  return x.reduce((a, b)=> a * b, 1);
+  }
+
 // tuesday 21 march 2023
 // debugging 
 
