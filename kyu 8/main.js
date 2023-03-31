990fbf37f518bbe710a0ddfa639f44302bad2c59
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-03-31 08:44:43 -0400

    march31

--- kyu 8/main.js
+++ kyu 8/main.js
@@ -161,3 +161,11 @@ function removeChar(str){
 function noSpace(x){
 return x.split(" ").join("")
 }
+
+//Friday March 2023
+//You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+//Array can contain numbers or strings. X can be either.
+//Return true if the array contains the value, false if not.
+function check(a,x){
+  return a.includes(x);
+};
