7cec343809a80e26cf4967df644dd5771322e522
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-04-06 21:36:08 -0400

    april6

--- kyu 8/main.js
+++ kyu 8/main.js
@@ -212,3 +212,16 @@ function findShort(s){
 return Math.min(...s.split(" ").map (s => s.length));
 }
 // return a math.min methon targeting the string and slip method to calculapte the lenght of the strings
+// april 6 2023
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+//2 -> 3 (1 + 2)
+//8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+var summation = function (num) {
+  let result = 0;
+  for (var i = 1; i <= num; i++) {
+    result += i;
+  }
+  
+  return result;
+}
