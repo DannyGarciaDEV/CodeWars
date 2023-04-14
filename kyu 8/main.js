56ed44efbacd608755a24e6220df662c39519d6c
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-04-13 20:06:02 -0400

    april13

--- kyu 8/main.js
+++ kyu 8/main.js
@@ -266,3 +266,11 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   let total = (laLigaGoals + copaDelReyGoals + championsLeagueGoals)
   return total
 }
+
+//april 13 
+//Grasshopper - Debug sayHello
+//The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+function sayHello(name) {
+  return `Hello, ${name}`
+ 
+}
