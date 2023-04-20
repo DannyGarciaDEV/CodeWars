4a77456fa46a7d640b9256a251b82847e772cd50
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-04-20 15:15:28 -0400

    pushing

--- kyu 7/main.js
+++ kyu 7/main.js
@@ -4,3 +4,6 @@
 //nput: 42145 Output: 54421
 //Input: 145263 Output: 654321
 //Input: 123456789 Output: 987654321
+function descendingOrder(n){
+    return parseInt(String(n).split('').sort().reverse().join(''))
+  }
