de4178eddf267207e265ae9fdf1058eedd389a54
your_github_DannyGarciaDEV <dannygarciadev@gmail.com>
2023-05-09 22:09:25 -0400

    may9

--- kyu 7/main.js
+++ kyu 7/main.js
@@ -131,6 +131,9 @@ function solution(start, finish){
 }
 
 //may 8
+// in this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+// For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
 
 function repeats(arr){
   // first filter the array to only get the items occuring once, aka filter for items where indexOf values = lastIndexOf value
@@ -139,3 +142,27 @@ function repeats(arr){
   //  then reduce to get the sum of the two numbers
    .reduce((a, b) => a + b);
 };
+
+
+//may9
+// The following code is not giving the expected results. Can you debug what the issue is?
+
+// The following is an example of data that would be passed in to the function.
+
+// var data = [
+//   {name: 'Joe', age: 20},
+//   {name: 'Bill', age: 30},
+//   {name: 'Kate', age: 23}
+// ]
+
+// getNames(data) // should return ['Joe', 'Bill', 'Kate']
+
+// function getNames(data){
+//   return data.map(function(item){item.name});
+// }
+
+function getNames(data){
+  return data.map(function(item){return item.name});
+
+
+}
